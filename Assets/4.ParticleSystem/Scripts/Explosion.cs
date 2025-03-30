c82d7fe4a0bd19c1d6964dd81dfaf7239720b0ae
nasonkin-aa <game-akaynt@mail.ru>
2025-03-30 17:03:46 +1000

    Add new material

--- Assets/4.ParticleSystem/Scripts/Explosion.cs
+++ Assets/4.ParticleSystem/Scripts/Explosion.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-   
+   public ParticleSystem ps;
+   public GameObject Tnt;
+
+
+   private void OnCollisionEnter2D(Collision2D other)
+   {
+      Debug.Log(other.gameObject.name);
+      GetComponent<Collider2D>().enabled = false;
+      ps.Play();
+      Tnt.SetActive(false);
+      
+   }
+
+
+
+
 }
